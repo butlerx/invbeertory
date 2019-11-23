da2e8ca0373f4fa25e6ba7a923354c0c2e18cedd
butlerx <butlerx@notthe.cloud>
2019-11-23 17:27:27 +0000

    remove unneeded dep

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,18 +20,15 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name,
+        short_name: name,
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     {
