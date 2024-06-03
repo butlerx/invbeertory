20e9b6cc54adcc8970328cdb0fe11fef97b8f2b9
Cian Butler <butlerx@notthe.cloud>
2024-06-03 22:06:48 +0100

    Fix table filtering

--- src/components/table.rs
+++ src/components/table.rs
@@ -2,8 +2,8 @@ use crate::{app::Route, formatting, storage::Beer};
 use implicit_clone::unsync::{IArray, IString};
 use web_sys::HtmlInputElement;
 use yew::{
-    classes, function_component, html, use_state, Callback, Html, InputEvent, Properties,
-    TargetCast,
+    classes, function_component, html, use_effect_with_deps, use_state, Callback, Html, InputEvent,
+    Properties, TargetCast,
 };
 use yew_router::prelude::Link;
 
@@ -29,12 +29,41 @@ pub fn data_grid(props: &Props) -> Html {
     let sort_by = use_state(|| None);
     let ascending = use_state(|| true);
     let filter_inputs = use_state(|| IArray::from(vec![IString::default(); HEADERS.len()]));
-    let data = use_state(|| data.clone());
+    let filtered_data = use_state(|| data.clone());
+
+    {
+        let data = data.clone();
+        let filter_inputs = filter_inputs.clone();
+        let filtered_data = filtered_data.clone();
+
+        use_effect_with_deps(
+            move |filter_inputs| {
+                let lower_filters = filter_inputs
+                    .iter()
+                    .map(|f| f.to_lowercase())
+                    .collect::<Vec<_>>();
+
+                let new_filtered_data = data
+                    .iter()
+                    .filter(|beer| {
+                        HEADERS.iter().enumerate().all(|(index, &(header, _))| {
+                            lower_filters[index].is_empty()
+                                || beer.filter_check(header, &lower_filters[index])
+                        })
+                    })
+                    .collect::<IArray<_>>();
+
+                filtered_data.set(new_filtered_data);
+                || ()
+            },
+            filter_inputs,
+        );
+    }
 
     let on_sort = {
         let sort_by = sort_by.clone();
         let ascending = ascending.clone();
-        let data = data.clone();
+        let filtered_data = filtered_data.clone();
         Callback::from(move |(index, header): (usize, &str)| {
             let current_sort = *sort_by;
             let mut new_ascending = true;
@@ -46,7 +75,7 @@ pub fn data_grid(props: &Props) -> Html {
             sort_by.set(Some(index));
             ascending.set(new_ascending);
 
-            let mut sorted_data = data.to_vec();
+            let mut sorted_data = filtered_data.to_vec();
             sorted_data.sort_by(|a, b| {
                 if new_ascending {
                     a.compare_field(b, header)
@@ -54,32 +83,16 @@ pub fn data_grid(props: &Props) -> Html {
                     b.compare_field(a, header)
                 }
             });
-            data.set(sorted_data.into());
+            filtered_data.set(sorted_data.into());
         })
     };
 
     let on_filter_change = {
-        let data = data.clone();
         let filter_inputs = filter_inputs.clone();
-        Callback::from(move |(index, header, value): (usize, &str, String)| {
+        Callback::from(move |(index, value): (usize, String)| {
             let mut new_filters = filter_inputs.to_vec();
             new_filters[index] = value.into();
-            filter_inputs.set(new_filters.clone().into());
-
-            let lower_filters = new_filters
-                .iter()
-                .map(|f| f.to_lowercase())
-                .collect::<Vec<_>>();
-
-            let new_filtered_data = data
-                .iter()
-                .filter(|beer| {
-                    lower_filters
-                        .iter()
-                        .all(|filter| filter.is_empty() || beer.filter_check(header, filter))
-                })
-                .collect::<IArray<_>>();
-            data.set(new_filtered_data);
+            filter_inputs.set(new_filters.into());
         })
     };
 
@@ -96,7 +109,7 @@ pub fn data_grid(props: &Props) -> Html {
                 let on_filter_change = on_filter_change.clone();
                 Callback::from(move |e: InputEvent| {
                     let input: HtmlInputElement = e.target_unchecked_into();
-                    on_filter_change.emit((index, header, input.value()))
+                    on_filter_change.emit((index, input.value()))
                 })
             };
 
@@ -111,13 +124,15 @@ pub fn data_grid(props: &Props) -> Html {
                         name={format!("filter-{}", header)}
                         aria-label={format!("Filter by {}", header)}
                         oninput={on_input}
+                        value={filter_inputs[index].to_string()}
                         min="0"
                     />
                 </div>
             }
-        });
+        })
+        .collect::<Html>();
 
-    let rows = data.iter().map(|beer| {
+    let rows = filtered_data.iter().map(|beer| {
         html! {
             <div class={classes!("grid-row")}>
                 <div class={classes!("grid-cell")}>
@@ -136,12 +151,12 @@ pub fn data_grid(props: &Props) -> Html {
                 <div class={classes!("grid-cell", "right")}>{beer.purchased}</div>
             </div>
         }
-    });
+    }).collect::<Html>();
 
     html! {
         <div class={classes!("data-grid")}>
-            <div class={classes!("grid-header")}>{ for header_cells }</div>
-            { for rows }
+            <div class={classes!("grid-header")}>{ header_cells }</div>
+            { rows }
         </div>
     }
 }
